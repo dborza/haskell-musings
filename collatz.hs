e4ea5ec27ad0bdf67020006bf1e3cd9d1e2fd9af
Dan Borza <dborza@kno.com>
2013-09-30 10:39:20 +0300

    added file with basic functions

---
+++ collatz.hs
@@ -0,0 +1,35 @@
+-- ye olde quicksorte
+quicksort :: (Integral a) => [a] -> [a]    
+quicksort [] = []    
+quicksort (x:xs) = quicksort (filter (<=x) xs) ++ [x] ++ quicksort (filter (>x) xs)
+
+-- Some funky function, also known at 'Collatz' function
+collatz :: (Integral a) => a -> a
+collatz x = if (even x) then (x `div` 2) 
+			else (3 * x + 1)
+
+--	Defines a collatz chain, given an integer. Supposedly all collatz chains end in '1'.	
+collatz_chain :: (Integral a) => a -> [a]
+collatz_chain 1 = [1]
+collatz_chain x = x : (collatz_chain (collatz x))
+
+-- How many collatz_chains starting from 1 .. n have a length greater than 15?
+collatz_chain_sum :: (Integral a) => a -> Int
+collatz_chain_sum x = length (filter (>15) (map length (map collatz_chain [1..x])))
+ 
+min' :: (Integral a) => a -> a -> a
+min' x y = if (x <= y) then x 
+			else y
+
+-- determine the minimum from a list
+min_list :: (Integral a) => [a] -> a
+min_list [x] = x
+min_list (x:hx) = min' x 2
+	
+-- another definition of the elem function. Checks if a given element is part of a list
+elem' e list = foldl (\acc x -> acc || x == e) False list
+
+-- implement the map function using a fold	
+map' f list = foldl (\acc x -> acc ++ [f(x)]) [] list
+
+main = print (map' (+1) [1,2,3])
